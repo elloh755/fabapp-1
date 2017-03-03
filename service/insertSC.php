ff8a4485a49393f6661f83e53237f2f99e2f9199
Abdul Mannan <abdul.mannan@mavs.uta.edu>
2017-03-02 20:37:00 -0600

    I added a new page for insertion status
    Tickets can now be added

---
+++ service/insertSC.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ *   CC BY-NC-AS UTA FabLab 2016-2017
+ *   FabApp V 0.9
+ */
+include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/header.php');
+
+?>
+<title><?php echo $sv['site_name'];?> Admin Base</title>
+<?php if ($staff) if($staff->getRoleID() < 7){
+    //Not Authorized to see this Page
+    header('Location: /index.php');
+}?>
+
+
+<?php 
+
+$fieldReport= "Your service call has been submitted, Thank you!";
+if ($mysqli->connect_errno) {
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	exit ();
+}
+
+
+
+$staffID = $staff->getOperator();
+$devID = $_POST['devGrp'];
+
+$slID = $_POST['optradio'];
+$solvedSt = "N";
+
+$scnotes = $_POST['notes'];
+$sctime = date("Y-m-d h:i:s");
+
+
+$toInsert = "INSERT INTO `service_call` (`sc_id`, `staff_id`, `d_id`, `sl_id`, `solved`, `sc_notes`, `sc_time`) VALUES (NULL, '".$staffID."', '".$devID."', '".$slID."', '".$solvedSt."', '".$scnotes."', '".$sctime."');";
+if(!$result = $mysqli->query($toInsert)){
+	$fieldReport = "Error in submitting";
+}
+header("refresh:5; url=/service/newTicket.php");
+
+?>
+
+
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header"><?php echo $fieldReport; ?></h1>
+            
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+        <p>This page will be redirected in 10 seconds </p>
+        
+        
+        </div>
+        <!-- /.col-lg-4 -->
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
+<?php
+//Standard call for dependencies
+include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/footer.php');
+?>
--- service/newTicket.php
+++ service/newTicket.php
@@ -1,72 +1,119 @@
 <?php
 /*
- *   CC BY-NC-AS UTA FabLab 2016-2017
- *   FabApp V 0.9
+ * CC BY-NC-AS UTA FabLab 2016-2017
+ * FabApp V 0.9
  */
-include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/header.php');
+include_once ($_SERVER ['DOCUMENT_ROOT'] . '/pages/header.php');
 ?>
+
+
+
+
 <title><?php echo $sv['site_name'];?> Admin Base</title>
-<?php if ($staff) if($staff->getRoleID() < 7){
-    //Not Authorized to see this Page
-    header('Location: /index.php');
-}?>
-<div id="page-wrapper">
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">Report Issue</h1>
-        </div>
-        <!-- /.col-lg-12 -->
-    </div>
-    <!-- /.row -->
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <i class="fa fa-ticket fa-fw"></i> New Ticket
-                </div>
-                <div class="panel-body">
-                   	<table class="table table-striped">
-                   		<tr><td>Device Group</td><td>
-							<select class="form-control">
-                    			<?php
-		                        	/* check connection */
-                    			if ($mysqli->connect_errno){
-                    					echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-                        		        exit();
-                            		}
-                            		if (!$result = $mysqli->query("SELECT dg_id,dg_name FROM device_group ORDER BY dg_name ASC")) {
-                            			echo "Table select failed: (" . $mysqli->errno . ") " . $mysqli->error;
-                            		}
-                            		while ($rows = mysqli_fetch_array($result)) {
-		                            	echo "<option value=" .$rows['dg_id']. ">" .$rows['dg_name']. "</option>";
-        		                    }?> 
-                				</select>
-                   		</td>
-		    	        <tr><td>Machine ID</td>
-		    	        <td><div class="form-group">
-							<textarea class="form-control" rows="1" id="machineID" style="resize:none"></textarea>
-						</div></td>
-        	    		<tr><td>Service Level</td>
-        	    		<td><label class="radio-inline"><input type="radio" name="optradio">Issue</label>
-							<label class="radio-inline"><input type="radio" name="optradio">Maintenance</label>
-							<label class="radio-inline"><input type="radio" name="optradio">Non-Operable</label></td>
-			            <tr><td>Notes:</td>
-			            <td><div class="form-group">
-							<textarea class="form-control" rows="5" id="notes" style="resize:none"></textarea>
-						</div></td>
-	            		<tr><td>Staff ID</td><td><?php echo $staff->getOperator();?></td>
-	            		<tr><td>Current Date</td><td><?php echo $date = date("m/d/Y h:i a", time());?></td>
-	            		<tr><td><input class="btn btn-primary pull-right" type="reset" value="Reset"></td><td><input class="btn btn-primary" type="submit" value="Submit"></td>
-          			</table>
-                </div>
-            </div>
-        </div>
-        <!-- /.col-lg-8 -->
-    </div>
-    <!-- /.row -->
-</div>
-<!-- /#page-wrapper -->
 <?php
-//Standard call for dependencies
-include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/footer.php');
+
+if ($staff->getRoleID () < 7) {
+	// Not Authorized to see this Page
+	header ( 'Location: /index.php' );
+}
+?>
+
+
+
+
+
+<div id="page-wrapper">
+	<div class="row">
+		<div class="col-lg-12">
+			<h1 class="page-header">Report Issue</h1>
+		</div>
+		<!-- /.col-lg-12 -->
+	</div>
+	<!-- /.row -->
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<i class="fa fa-ticket fa-fw"></i> New Ticket
+				</div>
+				
+			
+
+				<form method= "POST"  action="/service/insertSC.php">
+				<table class="table table-striped">
+						<tr>
+							<td>Device Group</td>
+							<td><select class="form-control" name="devGrp">
+                    			<?php
+																							/* check connection */
+																							if ($mysqli->connect_errno) {
+																								echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+																								exit ();
+																							}
+																							if (! $result = $mysqli->query ( "SELECT dg_id,dg_name FROM device_group ORDER BY dg_name ASC" )) {
+
+																								die("There was an error loading device_group ");
+																							}
+																							while ( $rows = mysqli_fetch_array ( $result ) ) {
+																								echo "<option value=" . $rows ['dg_id'] . ">" . $rows ['dg_name'] . "</option>";
+																							}
+																							?> 
+                				</select></td>
+						
+						</tr>
+						
+						<tr>
+							<td>Service Level</td>
+							<td><label class="radio-inline"><input type="radio"
+									name="optradio" value ="1">Issue</label> <label class="radio-inline"><input
+									type="radio" name="optradio" value="2">Maintenance</label> <label
+								class="radio-inline"><input type="radio" name="optradio" value="3">Non-Operable</label></td>
+						
+						</tr>
+						<tr>
+							<td>Notes:</td>
+							<td><div class="form-group">
+									<textarea class="form-control" rows="5" name="notes"
+										style="resize: none"></textarea>
+								</div></td>
+						
+						</tr>
+						<tr>
+							<td>Staff ID</td>
+							<td><?php echo $staff->getOperator();?></td>
+						
+						</tr>
+						<tr>
+							<td>Current Date</td>
+							<td><?php echo $date = date("m/d/Y h:i a", time());?></td>
+						
+						</tr>
+						<tr>
+							<td><input class="btn btn-primary pull-right" type="reset"
+								value="Reset"></td>
+							<td><input class="btn btn-primary" type="submit" value="Submit"></td>
+						</tr>
+					</table>
+				
+				
+				
+				
+				 
+				</form>
+					
+					
+				</div>
+			</div>
+		</div>
+		<!-- /.col-lg-8 -->
+	</div>
+	<!-- /.row -->
+
+<!-- /#page-wrapper -->
+
+
+
+<?php
+// Standard call for dependencies
+include_once ($_SERVER ['DOCUMENT_ROOT'] . '/pages/footer.php');
 ?>
