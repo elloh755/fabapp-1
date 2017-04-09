ec629d2f080c8ab81b3c116a20b7ce526fd1f03f
CetaneChris <christopher.raymond@mavs.uta.edu>
2017-04-09 11:22:54 -0500

    Large icons

--- service/ticketHistory.php
+++ service/ticketHistory.php
@@ -92,6 +92,7 @@ include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/header.php');
 											    WHERE operator = " . $cols['staff_id'])){
 												if($staffIcon->num_rows > 0){
 													$staffIcon = mysqli_fetch_array($staffIcon, MYSQLI_ASSOC);
+													echo "<td align='center' style='padding: 15px'><i class='fa fa-" . $staffIcon['icon'] . " fa-lg fa-fw'></i></td>";
 												}
 												else
 													echo "<td align='center' style='padding: 2px;'>Invalid User ID</td>";
